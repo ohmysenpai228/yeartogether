7b2bcca3c64bafcff5f7108cde73f07aaeb1797c
ohmysenpai228 <61983657+ohmysenpai228@users.noreply.github.com>
2024-03-18 22:45:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "І я буду з тобою назавжди 😘";
+  gif.src =
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnlnY3EzaG84dGN5cG1tYmhydzY2MjhzcHlmc3lmYmpqNmdmcmZmMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qFmdpUKAFZ6rMobzzu/giphy.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
